4f3bc94fa92f1270b1c5f440e85ad6366796fa66
Arun Yogi <yogiarun290@gmail.com>
2026-01-14 09:14:55 +0545

    Basic Python

---
+++ day1.py
@@ -0,0 +1,15 @@
+print("hello world", "testing", "arun", 12, 3, 4, 5, 6)
+print("this is second line")
+
+# a=10
+Test = 11
+TEST = 10
+_test = 10
+test_arun = 10    # No space in variable instead we should use "__".
+test1 = 10        # Variables shouldnot be in number or start with number or symbols.
+# print(a,Test)
+
+a = 10
+a = 11
+a = 15
+print(a)  # It prints the latest value i.e. a=15
