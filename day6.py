866b4904bfb2490d61035fb0ce37881e6c506336
Arun Yogi <yogiarun290@gmail.com>
2026-01-23 10:06:04 +0545

    For loop

--- day6.py
+++ day6.py
@@ -1,35 +1,92 @@
-#Membership operator
+# membership opeartor
 
-#in , not in
-a=[1,2,3,4,5,6,7]
-print(10 in a)
+# in , not in
+a = [1,2,3,5,5,6,7]
 print(10 not in a)
 
-#is, is not (==)
-a=2
+
+#is , is not (==)
+a = ""
 print(a is None)
-#"" vs None
+# "" vs None
 
-for i in range(11):
-    if i%2==0:
+
+
+'''
+for i in <list, string, dict>:
+    print(i)
+    print(i)
+    print(i)
+    print(i)
+    print(i)
+    print(i)
+    print(i)
+    print(i)
+
+'''
+
+
+
+for i in "broadway":
+    print(i)
+
+
+
+for i in [1,2,3,4,5,6,7,8,9,10]:
+    if (i%2==0):
         print(f'{i} is even')
-print("-------------------------"*40)
+    else:
+        print(f'{i} is odd')
 
-a=[1,2,3,4,5,'arun',2,34,5] 
-for i in range(len(a)):
-    print(a[i])     
 
-    
-a=[1,2,3,"Hello",3,4,5,"Arun",2,3]
+
+# range(1,10,1)
+for i in range(1,10,1):
+    print(i)
+
+for i in range(10,1,-1):
+    print(i)
+
+
+
+
+print("------------")
+a = [1,2,3,"test",5,6,7]
+print(".....",len(a))
+
 for i in a:
-    if isinstance(i,str):
-    
+    print(i)
+
+
+print("........")
+for i in range(len(a)):
+    print(a[i])
+
+
+
+print("+++++++++++++++++++")
+a = [1,2,3,"test",5,6,7]
+for i in a:
+    if i == "test":
+        break
+    print(i)
+
+a = [1,2,3,"test",5,6,7]
+for i in a:
+    if i == "test":
         continue
     print(i)
-    
-print("-------------------------"*10)
+
+a = [1,2,"nepal",3,"test",5,6,7, "hello"]
+for i in a :
+    if isinstance(i, str):
+        continue
+    print(i)
+
+print("===================")
+
 for i in range(1,4):
     for j in range(5,8):
-        if j==6:
+        if j == 6:
             break
         print(i,j)
